ec4e3172a3d8ca5e50a49036cb9001b7c5a8777a
tataéfoda <jeangustavofut@gmail.com>
2024-10-10 08:51:27 -0300

    aparecendo em branco

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -2,12 +2,36 @@
 
 public partial class MainPage : ContentPage
 {
+	const int gravidade = 30;
+	const int tempoEntreframes = 25;
+	bool estaMorto = false;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+	void AplicaGravidade()
+	{
+		pipaaa.TranslationY += gravidade;
+	}
+	async Task Desenhar()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreframes);
+		}
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Desenhar();
+	}
+
+
 
 
 }
+
 
