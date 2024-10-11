c37e2435085caabad1a4b12667f9a6a85b1f0330
jeanzinho <jeangustavofut@gmail.com>
2024-10-11 11:03:20 -0300

    11/10

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -4,7 +4,10 @@ public partial class MainPage : ContentPage
 {
 	const int gravidade = 30;
 	const int tempoEntreframes = 25;
-	bool estaMorto = false;
+	bool estaMorto = true;
+	double larguraJanela=0;
+	double AlturaJanela=0;
+	int velocidade=10;
 
 	public MainPage()
 	{
@@ -19,15 +22,40 @@ public partial class MainPage : ContentPage
 		while (!estaMorto)
 		{
 			AplicaGravidade();
+			GerenciarCanos();
 			await Task.Delay(tempoEntreframes);
+		
 		}
 	}
-
-	protected override void OnAppearing()
+    protected override void OnSizeAllocated(double w, double h)
+    {
+        base.OnSizeAllocated(w, h);
+		larguraJanela= w;
+		AlturaJanela= h;
+    }
+	void GerenciarCanos()
 	{
-		base.OnAppearing();
+		imgCima.TranslationX -= velocidade;
+		imgBaixo.TranslationX -= velocidade;
+		if(imgBaixo.TranslationX <-larguraJanela)
+		{
+			imgBaixo.TranslationX =0;
+			imgCima.TranslationX =0;
+		}
+
+	}
+	void OnGameOverClicked (object s, TappedEventArgs a)
+	{
+		frameGameOver.IsVisible =false; 
+		Inicializar();
 		Desenhar();
 	}
+	void Inicializar()
+	{
+		estaMorto= false;
+		pipaaa.TranslationY= 0;
+	}
+
 
 
 
